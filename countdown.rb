f6e7ff6d9ed23e5bb1b8509f0138e94b067e7597
Kristin <kristinponsonby@gmail.com>
2021-02-09 13:57:57 -0500

    commit tree a7be8ad9b83b0bd9d75b98704f61cceed0fc9d5c

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+def countdown(argument)
+    x = 10
+    while x > 0
+      puts "#{x} SECOND(S)!"
+      x -= 1 
+    end
+      return "HAPPY NEW YEAR!"
+end
 
-def countdown
+
+def countdown_with_sleep(argument)
+    x = 10
+    while x > 0
+      puts "#{x} SECOND(S)!" 
+      x -= 1 
+      sleep(5)
+    end
+      return "HAPPY NEW YEAR!"
 end
